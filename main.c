8759caa34a1f4e534db0749ae09edc9e5a440400
Ximelele <druzbacky.martin@gmail.com>
2020-04-18 17:53:32 +0200

    zadefinovanie struktur

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define DLZSl 100
 
+typedef struct name{
+    char meno[DLZSl];
+    char surrname[DLZSl];
+}NAME;
+typedef struct herec{
+    NAME meno_her;
+    int rok_narod;
+    struct herec *dalsi;
+}HEREC;
+
+typedef struct film{
+    char nazov_filmu[DLZSl];
+    int vyroba;
+    NAME meno_rez;
+    HEREC herec;
+    struct film *dalsifilm;
+
+}FILM;
 int main() {
-    printf("Hello, World!\n");
+    FILM *zac, *akt;
+
+    zac=(FILM *)malloc(sizeof(FILM));
+    scanf("%s %d %s %s %s %s %d",zac->nazov_filmu,&zac->vyroba,zac->meno_rez.meno,zac->meno_rez.surrname,zac->herec.meno_her.meno,zac->herec.meno_her.surrname,&zac->herec.rok_narod);
+    zac->dalsifilm=NULL;
+    printf("%s %d %s %s %s %s %d",zac->nazov_filmu,zac->vyroba,zac->meno_rez.meno,zac->meno_rez.surrname,zac->herec.meno_her.meno,zac->herec.meno_her.surrname,zac->herec.rok_narod);
     return 0;
 }
